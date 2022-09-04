9b207d16003832fff20d96fa001f740dc251fa2a
ETIYACORP\murat.gulcu <murat.gulcu@etiya.com>
2022-09-05 01:35:56 +0300

    autowired annotation with Spring

--- src/main/java/com/muratgulcu/springfundamentals/component/User.java
+++ src/main/java/com/muratgulcu/springfundamentals/component/User.java
@@ -1,8 +1,36 @@
 package com.muratgulcu.springfundamentals.component;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component(value = "bean1")
 public class User {
 
+    @Autowired
+    private UserController userController;
+
+    private UserRepository userRepository;
+
+    private UserService userService;
+
+    private Advice advice;
+
+
+    public User(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Autowired
+    public void testMethod(Advice advice){
+        this.advice=advice;
+    }
 }
